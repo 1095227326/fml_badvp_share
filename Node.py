4ce16bbc462c3829b9f918a881142f5d7de6469e
u-Long <1258305124@qq.com>
2024-04-25 10:59:33 +0800

    实验进展md，globalopt，globallr0.1

--- Node.py
+++ Node.py
@@ -440,8 +440,10 @@ class Global_node():
         self.prompter.to(self.device)
         self.optimizer = torch.optim.Adam(self.prompter.parameters(),
                                           lr=args.server_learning_rate,
+                                          weight_decay=args.weight_decay) # TODO
+        self.gloptimizer = torch.optim.Adam(self.prompter.parameters(),
+                                          lr=args.server_learning_rate,
                                           weight_decay=args.weight_decay)
-
         self.scheduler = cosine_lr(
             self.optimizer, args.learning_rate, args.warmup, total_steps)
         self.criterion = torch.nn.CrossEntropyLoss().to(self.device)
@@ -511,8 +513,8 @@ class Global_node():
                 param.grad = global_weights_grad[name]
                 
         # 使用服务器端优化器进行一步更新
-        self.optimizer.step()
-        self.optimizer.zero_grad()  # 准备下一轮的更新
+        self.gloptimizer.step()
+        self.gloptimizer.zero_grad()  # 准备下一轮的更新
         # self.scheduler.step()
         
         return
